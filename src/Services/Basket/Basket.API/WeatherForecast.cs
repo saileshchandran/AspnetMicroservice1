69ff39b22e28f116e89f1266009afff020f18999
sailesh chandran <sailesh.c@ibsfintech.com>
2023-08-10 13:04:54 +0530

    Added BasketService API

--- src/Services/Basket/Basket.API/WeatherForecast.cs
+++ src/Services/Basket/Basket.API/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace Catalog.API
+using System;
+
+namespace Basket.API
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace Catalog.API
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }

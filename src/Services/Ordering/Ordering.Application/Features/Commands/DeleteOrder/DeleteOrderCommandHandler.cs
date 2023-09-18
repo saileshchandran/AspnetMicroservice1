bdf08104753527f2d9fcb5ac7357987250af609f
sailesh chandran <sailesh.c@ibsfintech.com>
2023-09-18 17:25:02 +0530

    Update and Delete orders

---
+++ src/Services/Ordering/Ordering.Application/Features/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Features.Commands.UpdateOrder;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete= await _orderRepository.GetByIdAsync(request.Id);
+            if (orderToDelete == null)
+            {
+                _logger.LogError("Order does not exist in the database.");
+                //throw new NotFoundException(nameof(Order), request.Id);
+            }
+            else
+            {
+                _mapper.Map(request, orderToDelete, typeof(UpdateOrderCommand), typeof(Order));
+                await _orderRepository.DeleteAsync(orderToDelete);
+                _logger.LogInformation($"Order {orderToDelete.Id} is successfully updated.");
+            }
+            return Unit.Value;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Features/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -16,15 +16,18 @@ namespace Ordering.Application.Features.Queries.GetOrdersList
         private readonly IOrderRepository _orderRepository;
         private readonly IMapper _mapper;
 
+
+
         public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
         {
             _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
-        public Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
         {
-            
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
         }
     }
 }
